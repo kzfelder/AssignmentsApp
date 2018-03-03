25dad8a4e4e4b5363eae3b830d3a6b67eaf878ee
kzfelder <35270718+kzfelder@users.noreply.github.com>
2018-03-03 12:08:36 -0600

    Output the current date-time and output tomorrow's date using a formatter

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,32 @@
-public class Main {
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-    public static void main(String[] args) {
+public class Main
+{
+
+    public static void main(String[] args)
+    {
         System.out.println("\n\nHello, AssignmentsApp!\n");
+
+        //Output the current date-time
+        LocalDateTime today = LocalDateTime.now();
+        System.out.println("Current date-time: " + today);
+
+        // Output tomorrow's date using a formatter
+        LocalDateTime tomorrow = addDaysToDateTime(today,1);
+        String formattedTomorrow = dateTimeFormatter(tomorrow);
+        System.out.println("Tomorrow: " + formattedTomorrow);
+    }
+
+    private static LocalDateTime addDaysToDateTime(LocalDateTime dateTime, int numDays)
+    {
+        return dateTime.plusDays(numDays);
+    }
+
+    private static String dateTimeFormatter(LocalDateTime dateTime)
+    {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yy-MM-dd HH:mm:ss");
+        return dateTime.format(formatter);
     }
 }
+
